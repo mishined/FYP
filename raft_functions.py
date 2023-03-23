bc3f88ddc9e93cd0bdcf38c540b560de7c0fc7ec
Mihaela <92406602+mishined@users.noreply.github.com>
2023-03-23 17:11:21 +0000

    updated

--- raft_functions.py
+++ raft_functions.py
@@ -49,40 +49,93 @@ def remap_backward(image, flow):
     x = np.linspace(0, image.shape[0], image.shape[0])
     y = np.linspace(0, image.shape[1], image.shape[1])
     xv, yv = np.meshgrid(x,y)
-    map_x = flow[:, :, 0] + xv
-    map_y = flow[:, :, 1] + yv
+    map_x = flow[:, :, 0] + yv
+    map_y = flow[:, :, 1] + xv
 
     mapped_img = cv2.remap(image, map_x.astype(np.float32), map_y.astype(np.float32), cv2.INTER_LINEAR) #pull back mapping to avoid holes in the image
     return mapped_img
 
 
-def draw_process_evaluate(image1, image2, mapped):
+def draw_process_evaluate(image_from, image_to, mapped):
     plt.subplot(1,3,1)
-    plt.imshow(image1)
+    plt.imshow(image_from)
     plt.subplot(1,3,2)
-    plt.imshow(image2)
+    plt.imshow(image_to)
     plt.subplot(1,3,3)
     plt.imshow(mapped, cmap = "gray")
     plt.show()
-    print('Structural similarity between the 2 images:', sm.structural_similarity(image2[:, :, 0], mapped[:,:,0]))
+    print('Structural similarity between the 2 images:', sm.structural_similarity(image_to[:, :, 0], mapped[:,:,0]))
 
 
-def draw_remap_with_flow(image, image2, flow):
+def draw_remap_with_flow(image_from, image_to, flow):
     flow[np.isnan(flow)] = 0
-    x = np.linspace(0, image.shape[0], image.shape[0])
-    y = np.linspace(0, image.shape[1], image.shape[1])
+    x = np.linspace(0, image_from.shape[0], image_from.shape[0])
+    y = np.linspace(0, image_from.shape[1], image_from.shape[1])
     xv, yv = np.meshgrid(x,y)
     map_x = flow[:, :, 0] + xv
     map_y = flow[:, :, 1] + yv
 
-    mapped_img = cv2.remap(image, map_x.astype(np.float32), map_y.astype(np.float32), cv2.INTER_LINEAR) #pull back mapping to avoid holes in the image
+    mapped_img = cv2.remap(image_from, map_x.astype(np.float32), map_y.astype(np.float32), cv2.INTER_LINEAR) #pull back mapping to avoid holes in the image
     
     plt.subplot(1,3,1)
-    plt.imshow(image)
+    plt.imshow(image_from)
     plt.subplot(1,3,2)
-    plt.imshow(image2)
+    plt.imshow(image_to)
     plt.subplot(1,3,3)
     plt.imshow(mapped_img, cmap = "gray")
     plt.show()
 
-    print('Second image warped into the first:', sm.structural_similarity(image2[:, :, 0], mapped_img[:,:,0]))
+    print('Second image warped into the first:', sm.structural_similarity(image_to[:, :, 0], mapped_img[:,:,0]))
+
+
+from torchvision.io import read_video
+
+def load_batches():
+    pass
+
+
+def preprocess(img1_batch, img2_batch, transforms):
+    # img1_batch = F.resize(img1_batch, size=[520, 960], antialias=False)
+    # img2_batch = F.resize(img2_batch, size=[520, 960], antialias=False)
+    img1_batch = F.resize(img1_batch, size=[352, 352], antialias=False) # resize frames to ensure they are divisable by 8 
+    img2_batch = F.resize(img2_batch, size=[352, 352], antialias=False)
+    return transforms(img1_batch, img2_batch)
+
+
+from torchvision.models.optical_flow import Raft_Large_Weights
+from torchvision.models.optical_flow import raft_large
+
+def raft(img1_batch, img2_batch, raft_ = 'large', device = 'cpu'):
+    # set the weights
+    weights = Raft_Large_Weights.DEFAULT
+    transforms = weights.transforms()
+    # prepare the images for the model
+    img1_batch, img2_batch = preprocess(img1_batch, img2_batch, transforms)
+    # create model
+    model = raft_large(weights=Raft_Large_Weights.DEFAULT, progress=False).to(device)
+    model = model.eval()
+
+    # get list of predictions
+    list_of_flows = model(img1_batch.to(device), img2_batch.to(device))
+    # get the last one - most accurate
+    predicted_flow = list_of_flows[-1][0]
+
+    return predicted_flow
+
+
+    # flow_img = flow_to_image(predicted_flow).to("cpu")
+
+
+def evaluate_raft(img1_batch, img2_batch, predicted_flows, remapping = 'forward'):
+    images1 = convert_images(img1_batch) # all frames 1
+    images2 = convert_images(img2_batch) # all frames 2
+
+    flows = convert_flow(predicted_flows)
+
+    if remapping == 'forward':
+        mapped = remap_forward(images1, flows)
+        return [sm.structural_similarity(images2[:, :, 0], mapped[:,:,0])]
+
+    else:
+        mapped = remap_backward(images2, flows)
+        return [sm.structural_similarity(images1[:,:,0], mapped[:,:,0])]
