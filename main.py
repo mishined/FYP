b324460b766f2a4742fede69ee2903884fd00638
Mihaela <92406602+mishined@users.noreply.github.com>
2023-05-01 19:35:56 +0100

    update

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ import pickle
 
 if __name__ == '__main__':
 
-    video_dataset = MRIDataset("/Users/Misha/OneDrive - University of Sussex/FYP/Participants/VIDEOS_ALL/*.mp4", 
-                               transform=None)
+    # video_dataset = MRIDataset("/Users/men22/OneDrive - University of Sussex/FYP/Participants/VIDEOS_ALL/*.mp4", 
+                            #    transform=None)
 
-    video_dataset_transformed = MRIDataset("/Users/Misha/OneDrive - University of Sussex/FYP/Participants/VIDEOS_ALL/*.mp4", 
+    video_dataset_transformed = MRIDataset("/Users/men22/OneDrive - University of Sussex/FYP/Participants/VIDEOS_ALL/*.mp4", 
                                transform=transforms.Compose([Rescale(256),
                                RaftTransforms()]))
     
@@ -27,43 +27,40 @@ if __name__ == '__main__':
     # evaluate_raft_2(video_dataset_transformed[0][0], video_dataset_transformed[0][1], flows[0])
 
 
+    from torch.utils.data import Subset
+    data_subset = Subset(video_dataset_transformed, range(1280, 1370))
+    del video_dataset_transformed
 
     # print(video_dataset_transformed.paths)
 
-    video_dataloader = get_dataloader(video_dataset_transformed, batch_size=1)
+    video_dataloader = get_dataloader(data_subset, batch_size=1)
 
-    
+    # print(len(data_subset))
+
+    # for i, videos in enumerate(video_dataloader):
+    #     sample1, sample2 = videos
+    #     print(type(sample1))
     flows = []
     for i, videos in enumerate(video_dataloader):
-        if i == 30: break
+
         # print((videos[0].shape))
         sample1, sample2 = videos
+        del videos
         if isinstance(sample1, list): continue
         print(i)
         # print(type(sample1))
         # if isinstance(sample1[0], list): continue
         flow = raft(sample1[0], sample2[0])
+        del sample1, sample2
         flows.append(flow)
-
-
-    # for i, videos in enumerate(video_dataloader):
-        
-    #     # print((videos[0].shape))
-    #     sample1, sample2 = videos
-    #     if isinstance(sample1, list): continue
-    #     print(i)
-    #     # print(type(sample1))
-    #     # if isinstance(sample1[0], list): continue
-    #     flow = raft(sample1[0], sample2[0])
-    #     flows.append(flow)
-     
 
     print(len(flows))
     
-    # with open('flows2.pickle','wb') as temp:
-    #     pickle.dump(flows, temp)
+    # # with open('flows2.pickle','wb') as temp:
+    # #     pickle.dump(flows, temp)
 
     import pickle
-    with open('flows2.pickle','wb') as temp:
-        pickle.dump([video_dataset, video_dataset_transformed, flows], temp)
+    with open('flows16.pickle','wb') as temp:
+        pickle.dump([data_subset, flows], temp)
 
+#flows 3 onwards are all
